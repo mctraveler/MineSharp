50381ff3b8a7fd784fb62f051668d05505d8b848
Nuxas Franc Mite <nuxas@mctraveler.eu>
2014-09-24 00:47:14 +0200

    Prepare for new auth

---
+++ MineProxy/Network/AuthResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MineProxy.Network
+{
+    /// <summary>
+    /// From https://sessionserver.mojang.com/session/minecraft/hasJoined?username=username&serverId=hash
+    /// </summary>
+    public class AuthResponse
+    {
+        public Guid id;
+
+        public List<Property> properties = new List<Property>();
+
+        public class Property
+        {
+            public string name;
+            public byte[] value;
+            public byte[] signature;
+        }
+    }
+}
+
