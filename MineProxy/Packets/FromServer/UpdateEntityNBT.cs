aa1a638ab4a07d063950839e6684dd1e0ccdb4a3
Nuxas Franc Mite <nuxas@mctraveler.eu>
2014-09-28 01:23:49 +0200

    NBT tag parsing of entity metadata

--- MineProxy/Packets/FromServer/UpdateEntityNBT.cs
+++ MineProxy/Packets/FromServer/UpdateEntityNBT.cs
@@ -1,23 +1,30 @@
 using System;
 using MineProxy.Data;
 using MiscUtil.IO;
+using MineProxy.NBT;
 
 namespace MineProxy.Packets
 {
-    public class UpdateEntityNBT : PacketFromServer
+    public class UpdateEntityNBT : PacketFromServer, IEntity
     {
         public const byte ID = 0x49;
 
         public override byte PacketID { get { return ID; } }
 
+        public int EID { get; set; }
+
+        public Tag Tag { get; set; }
+
         protected override void Parse(EndianBinaryReader r)
         {
-            throw new NotImplementedException();
+            EID = ReadVarInt(r);
+            Tag = Tag.ReadTag(r);
         }
 
         protected override void Prepare(EndianBinaryWriter w)
         {
-            throw new NotImplementedException();
+            WriteVarInt(w, EID);
+            Tag.Write(w);
         }
     }
 }
