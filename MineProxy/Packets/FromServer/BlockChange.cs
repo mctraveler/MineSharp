76025d9a8773c5888548bfee6259de0889217534
Nuxas Franc Mite <nuxas@mctraveler.eu>
2014-12-07 09:52:07 +0100

    New BlockChange Format

--- MineProxy/Packets/FromServer/BlockChange.cs
+++ MineProxy/Packets/FromServer/BlockChange.cs
@@ -8,9 +8,9 @@ namespace MineProxy.Packets
         public const byte ID = 0x23;
 
         public override byte PacketID { get { return ID; } }
-		
+
         public CoordInt Position { get; set; }
-		
+
         public BlockID BlockType { get; set; }
 
         public byte Metadata { get; set; }
@@ -19,7 +19,7 @@ namespace MineProxy.Packets
         {
             return string.Format("[BlockChange: X={0}, Y={1}, Z={2}, Type={3}]", Position.X, Position.Y, Position.Z, BlockType);
         }
-		
+
         public BlockChange()
         {
         }
@@ -37,7 +37,7 @@ namespace MineProxy.Packets
                 Metadata = (byte)block.Meta;
             }
         }
-        
+
         public BlockChange(CoordInt pos, byte blockID, byte meta)
         {
             if (pos == null)
@@ -46,7 +46,7 @@ namespace MineProxy.Packets
             BlockType = (BlockID)blockID;
             Metadata = meta;
         }
-        
+
         public BlockChange(CoordInt pos, BlockID block)
         {
             if (pos == null)
@@ -58,19 +58,19 @@ namespace MineProxy.Packets
         protected override void Parse(EndianBinaryReader r)
         {
             Position = CoordInt.Read(r);
-            BlockType = (BlockID)ReadVarInt(r);
-            Metadata = r.ReadByte();
+            int val = ReadVarInt(r);
+            BlockType = (BlockID)(val >> 4);
+            Metadata = (byte)(val & 0x0F);
             #if DEBUGPACKET
             if (BlockType.ToString() == ((int)BlockType).ToString())
                 throw new NotImplementedException(BlockType.ToString());
             #endif
         }
-		
+
         protected override void Prepare(EndianBinaryWriter w)
         {
             Position.Write(w);
-            WriteVarInt(w, (int)BlockType);
-            w.Write(Metadata);
+            WriteVarInt(w, ((int)BlockType << 4) | (Metadata & 0x0F));
         }
     }
 }
